056bae063fbf6c8a7816d21e6b31fb329090a01f
koshaliev <default>
2025-01-26 18:44:46 +0300

    Переместил объект, хранящий Ответ в случае ошибки

--- src/OzonSellerApi/Dto/Responses/Common/FailureResponseDto.cs
+++ src/OzonSellerApi/Dto/Responses/Common/FailureResponseDto.cs
@@ -1,22 +1,37 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace OzonSellerApi;
+namespace OzonSellerApi.Dto.Responses.Common;
 public record FailureResponseDto
 {
+    /// <summary>
+    /// Код ошибки.
+    /// </summary>
     [JsonPropertyName("code")]
     public int Code { get; set; }
 
+    /// <summary>
+    /// Дополнительная информация об ошибке.
+    /// </summary>
     [JsonPropertyName("details")]
     public List<Detail> Details { get; set; } = [];
 
+    /// <summary>
+    /// Описание ошибки.
+    /// </summary>
     [JsonPropertyName("message")]
     public string Message { get; set; } = string.Empty;
 
     public class Detail
     {
+        /// <summary>
+        /// Тип протокола передачи данных.
+        /// </summary>
         [JsonPropertyName("typeUrl")]
         public string TypeUrl { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Значение ошибки.
+        /// </summary>
         [JsonPropertyName("value")]
         public string Value { get; set; } = string.Empty;
     }
